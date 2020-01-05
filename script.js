8e3b838eaef29c5333b9821b4cc06f6b8220bd3f
Jsegel <segeljosh@gmail.com>
2020-01-05 17:50:13 -0600

    adding weather API

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 $(".citybtn").on("click", function (event) {
     event.preventDefault();
-    var city = $(".inputcity").val();
-    console.log(city)
-    console.log("button working")
-    
+    weatherCall()
 })
 
-var city = $(".inputcity").val();
-console.log(city);
-var weatherurl = "api.openweathermap.org/data/2.5/forecast?q=" + city + ",us&appid=ab2b42dffa09ca26ec55a2393bf2aa3d"
+function weatherCall() {
+    var city =$(".inputcity").val();
+    var weatherurl = "api.openweathermap.org/data/2.5/forecast?q=" + city + ",us&appid=ab2b42dffa09ca26ec55a2393bf2aa3d"
+    console.log(city);
+    $.ajax({
+        url:weatherurl,
+        format: "json",
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        console.log("response")
+    })
+}
+
+
