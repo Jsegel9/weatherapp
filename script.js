44d27fde2fa98a596c8f744712757c1c4a65de0a
Jsegel <segeljosh@gmail.com>
2020-01-05 23:09:17 -0600

    adding current forecast elements

--- script.js
+++ script.js
@@ -13,9 +13,12 @@ function weatherCall() {
     }).then(function (response) {
         var today = new Date();
         var date = (today.getMonth()+1) + "/" + today.getDate() + "/" + today.getFullYear();
+        var foreimg = response.weather[0].icon
+        var imgsrc = "http://openweathermap.org/img/wn/" + foreimg + ".png"
         console.log(response);
-        console.log(response.name)
+        // console.log(response.weather[0].icon);
         $(".forecasthead").html(response.name + " (" + date + ")" + "<img src='' class='foreimg'>");
+        $(".foreimg").attr("src", imgsrc);
         $(".currentweather").text(JSON.stringify(response));
     })
 }
