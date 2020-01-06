a45a9279b80b8902d8ad138b95028a32d510183e
Jsegel <segeljosh@gmail.com>
2020-01-06 00:13:05 -0600

    weather elements

--- script.js
+++ script.js
@@ -3,12 +3,6 @@ $(".citybtn").on("click", function (event) {
     weatherCall();
 })
 
-// $(".histbtn").on("click", function (event) {
-//     event.preventDefault();
-//     // weatherCall2();
-//     console.log((".histbtn").val());
-// })
-
 function weatherCall() {
     var city =$(".inputcity").val();
     var curweathurl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + ",us&appid=ab2b42dffa09ca26ec55a2393bf2aa3d"
