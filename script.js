e84eeb4e2154773f7f04cf819b45f85284674f82
Jsegel <segeljosh@gmail.com>
2020-01-05 22:59:30 -0600

    cleaning up weather call

--- script.js
+++ script.js
@@ -6,12 +6,16 @@ $(".citybtn").on("click", function (event) {
 function weatherCall() {
     var city =$(".inputcity").val();
     var curweathurl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + ",us&appid=ab2b42dffa09ca26ec55a2393bf2aa3d"
-    console.log(city);
+    var foreurl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + ",us&appid=ab2b42dffa09ca26ec55a2393bf2aa3d"
     $.ajax({
         url: curweathurl,
         method: "GET"
     }).then(function (response) {
+        var today = new Date();
+        var date = (today.getMonth()+1) + "/" + today.getDate() + "/" + today.getFullYear();
         console.log(response);
+        console.log(response.name)
+        $(".forecasthead").html(response.name + " (" + date + ")" + "<img src='' class='foreimg'>");
         $(".currentweather").text(JSON.stringify(response));
     })
 }
