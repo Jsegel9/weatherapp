0c05eefb8caabaa73e9ba53e6c38376398149d0a
Jsegel <segeljosh@gmail.com>
2020-01-05 18:51:37 -0600

    fixed problem with weather

--- script.js
+++ script.js
@@ -5,11 +5,10 @@ $(".citybtn").on("click", function (event) {
 
 function weatherCall() {
     var city =$(".inputcity").val();
-    var weatherurl = "api.openweathermap.org/data/2.5/forecast?q=" + city + ",us&appid=ab2b42dffa09ca26ec55a2393bf2aa3d"
+    var weatherurl = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + ",us&appid=ab2b42dffa09ca26ec55a2393bf2aa3d"
     console.log(city);
     $.ajax({
-        url:weatherurl,
-        format: "json",
+        url: weatherurl,
         method: "GET"
     }).then(function (response) {
         console.log(response);
