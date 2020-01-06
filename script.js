30aeec74801001574f2c9b64fc2079793d7b29bf
Jsegel <segeljosh@gmail.com>
2020-01-06 00:50:44 -0600

    adding five day elements

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 $(".citybtn").on("click", function (event) {
     event.preventDefault();
     weatherCall();
+    // fiveDayCall();
 })
 
 function weatherCall() {
     var city =$(".inputcity").val();
     var curweathurl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + ",us&appid=ab2b42dffa09ca26ec55a2393bf2aa3d"
-    var foreurl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + ",us&appid=ab2b42dffa09ca26ec55a2393bf2aa3d"
+    var foreurl = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + ",us&appid=ab2b42dffa09ca26ec55a2393bf2aa3d"
     $.ajax({
         url: curweathurl,
         method: "GET"
@@ -27,7 +28,16 @@ function weatherCall() {
         $(".histbtn").text(response.name);
         $(".currentweather").text(JSON.stringify(response));
     })
+    $.ajax({
+        url: foreurl,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        $(".day1date").text(response.list[0].dt_txt);
+    })
 }
+
+
 $(".histbtn").on("click", function (event) {
     event.preventDefault();
     // weatherCall2();
